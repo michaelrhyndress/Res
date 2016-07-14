57e4739eb0a856359b874ea68ea23b05ab0f2625
Michael Rhyndress <michaelrhyndress@gmail.com>
2016-07-14 18:22:43 -0400

    starting fresh

--- web/static/src/js/resify/rootReducer.js
+++ web/static/src/js/resify/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import nameField from './_resumeFormElements/_fields/_name';
+// import nameField from './_resumeFormElements/_fields/_name';
 
 export default combineReducers({
-  [nameField.constants.NAME]: nameField.reducer
+  // [nameField.constants.NAME]: nameField.reducer
 });
